a4813f1b4ca6baa114f9a3b23c896065f102ddf4
kjha <kirti.jha@blackboard.com>
2019-07-28 19:50:32 -0700

    Intial commit

---
+++ ChatApplication/Handlers/WebSocketMessageHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+using ChatApplication.SocketsManager;
+
+namespace ChatApplication.Handlers
+{
+    public class WebSocketMessageHandler : SocketHandler
+    {
+        public override async Task OnConnected(WebSocket socket)
+        {
+            await base.OnConnected(socket);
+            var socketId = Connection.GetId(socket);
+            await SendMessageToAll(socketId +"Just joined the chat room ");
+        }
+
+        public override async Task Receive(WebSocket socket, WebSocketReceiveResult result, byte[] buffer)
+        {
+            var socketId = Connection.GetId(socket);
+            var message = socketId + "said: "+ Encoding.UTF8.GetString(buffer, 0, result.Count);
+            await SendMessageToAll(message);
+        }
+    }
+}
---
+++ ChatApplication/SocketsManager/ConnectionManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ChatApplication.SocketsManager
+{
+    public class ConnectionManager
+    {
+         private ConcurrentDictionary<string, WebSocket> connections = new ConcurrentDictionary<string, WebSocket>();
+
+        public WebSocket GetSocketById(string id)
+        {
+            return connections.FirstOrDefault(x => x.Key == id).Value;
+        }
+
+        public ConcurrentDictionary<string, WebSocket> GetAllConnections()
+        {
+            return connections;
+        }
+
+        public string GetId(WebSocket webSocket)
+        {
+            return connections.FirstOrDefault(x => x.Value == webSocket).Key;
+        }
+
+        public async Task RemoveSocketAsync(string id)
+        {
+            connections.TryRemove(id, out var socket);
+            await socket.CloseAsync(WebSocketCloseStatus.NormalClosure, "socket closed", CancellationToken.None);
+        }
+
+        public void AddSocket(WebSocket socket)
+        {
+            connections.TryAdd(GetConnectionId(), socket);
+        }
+
+        private string GetConnectionId()
+        {
+            return Guid.NewGuid().ToString("N");
+        }
+    }
+}
